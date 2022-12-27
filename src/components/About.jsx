ecf3bee3ac3e8f6746b518d895634db8bc82a45d
BlazerXXI <ruslandendik@gmail.com>
2022-12-27 21:48:04 +0200

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
   const AboutLink = "/";
   return (
     <section className="mb-[95px] sm:flex sm:flex-row flex-col-reverse container max-w-[1240px] m-auto px-2 text-center sm:text-left sm:gap-[85px] gap-10">
-      <div>
-        <img className="w-full" src={aboutImage} alt="About" />
+      <div className="flex-auto">
+        <img className="w-full " src={aboutImage} alt="About" />
       </div>
       <div className="sm:w-[456px] flex flex-col sm:mt-0 mt-10 justify-center">
         <h2 className="font-['Playfair_Display'] text-5xl font-[900]">
