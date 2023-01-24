b3936d44e698b87af16a4a09ba4e14834e651afa
BlazerXXI <ruslandendik@gmail.com>
2023-01-24 21:23:39 +0200

    fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 
 import bgSlide01 from "../img/pexels-expect-best-323780-res.jpg";
 
-import bgSlide02 from "../img/projects/projectsSix.jpg";
+import bgSlide02 from "../img/780-1150x739.jpg";
 
-import bgSlide03 from "../img/projects/projectsFive.jpg";
+import bgSlide03 from "../img/641-1150x739.jpg";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
@@ -52,10 +52,10 @@ const Hero = () => {
 
   return (
     <section className="hero relative container m-auto max-sm:flex max-sm:flex-col max-sm:gap-1">
-      <div className="lg:w-[691px]  max-sm:justify-end max-sm:flex max-sm:flex-col  lg:h-[550px] sm:w-[450px]   sm:mt-[-150px]  lg:mt-[-275px]   sm:bg-gradient-to-t sm:from-[#F4F6F5E5] sm:to-[#FFFFFFE5] sm:absolute sm:top-1/2 top-0  left-0  ml-0 lg:ml-[-100px] lg:pl-[100px] py-4 pl-4 sm:py-[60px] lg:py-[138px] sm:pr-4  z-20 sm:min-h-[423px]">
+      <div className="lg:w-[691px]  max-sm:justify-end max-sm:flex max-sm:flex-col  lg:h-[550px] sm:w-[450px]   sm:mt-[-150px]  lg:mt-[-275px]   sm:bg-gradient-to-t sm:from-[#F4F6F5E5] sm:to-[#FFFFFFE5] sm:absolute sm:top-1/2 top-0  left-0  ml-0 lg:ml-[-100px] lg:pl-[100px] py-4 pl-4 sm:py-[60px] lg:py-[138px] sm:pr-4  z-20">
         <h1
           id="titleHero"
-          className="font-[900]  text-[51px] font-['Playfair_Display'] "
+          className="font-[900] text-[51px] font-['Playfair_Display'] "
         >
           {" "}
         </h1>
@@ -68,7 +68,7 @@ const Hero = () => {
           src={sliderImage[1]}
           alt=" "
           id="heroBg"
-          className="filter bgSlide brightness-50 sm:brightness-100 sm:min-h-[862px] min-w-[400px]  w-full min-h-[400px]"
+          className="filter bgSlide brightness-50 sm:brightness-100  w-full"
         />
         <div className="absolute bottom-0 right-0  flex gap-4 py-2 px-2 sm:gap-[50px] sm:py-[16px] sm:px-[33px] bg-[#F4F6F5B2] ">
           <div onClick={() => setSlide(slide - 1)}>
