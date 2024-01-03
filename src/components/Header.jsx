2efd7ad4613dd75acd8edbfd3f3adb42dcaf6d39
BlazerXXI <ruslandendik@gmail.com>
2024-01-03 19:25:15 +0200

    fix: small fixs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,127 +3,131 @@ import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 
 const Header = () => {
-  const link = "./";
-  const [nav, setNav] = useState(true);
-  const handleNav = () => {
-    setNav(!nav);
-  };
-  return (
-    <header className="container m-auto">
-      <div className=" md:flex items-center md:text-center justify-between mx-2  min-h-[50px] mt-6 md:mb-6 mb-4 ">
-        <a
-          href={link}
-          className="font-[Barlow] text-[42px] font-semibold pr-10"
-        >
-          ArchiWeb
-        </a>
+	const link = "./";
+	const [navIsOpen, setNavIsOpen] = useState(true);
+	const handleNav = () => {
+		setNavIsOpen(!navIsOpen);
+	};
+	return (
+		<header className="container m-auto">
+			<div className=" md:flex items-center md:text-center justify-between mx-2  min-h-[50px] mt-6 md:mb-6 mb-4 ">
+				<a
+					href={link}
+					className="font-[Barlow] text-[42px] font-semibold pr-10"
+				>
+					ArchiWeb
+				</a>
 
-        <div
-          onClick={handleNav}
-          className="fixed z-50 top-0 rounded-full w-[45px] sm:w-[30px] h-[45px] sm:h-[30px] text-center justify-center items-center active:opacity-40 drop-shadow-md right-0 m-5 mt-7 flex md:hidden cursor-pointer hover:opacity-60 duration-300 ease-in-out"
-        >
-          {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
-        </div>
-        <div className="w-[45px] sm:hidden sm:w-[30px] h-[45px] sm:h-[30px] bg-[#ffffff80] z-40 rounded-full fixed top-0 right-0 m-5 mt-7"></div>
+				<div
+					onClick={handleNav}
+					className="fixed z-50 top-0 rounded-full w-[45px] sm:w-[30px] h-[45px] sm:h-[30px] text-center justify-center items-center active:opacity-40 drop-shadow-md right-0 m-5 mt-7 flex md:hidden cursor-pointer hover:opacity-60 duration-300 ease-in-out"
+				>
+					{!navIsOpen ? (
+						<AiOutlineClose size={25} />
+					) : (
+						<AiOutlineMenu size={25} />
+					)}
+				</div>
+				<div className="w-[45px] sm:hidden sm:w-[30px] h-[45px] sm:h-[30px] bg-[#ffffff80] z-40 rounded-full fixed top-0 right-0 m-5 mt-7"></div>
 
-        <nav className="md:gap-16 md:flex hidden mt-10 md:mt-0 justify-center items-center">
-          <ul className="grid grid-cols-2 sm:grid-cols-4 md:gap-24 gap-10 p-2 text-center">
-            <li>
-              <a
-                href={link}
-                className="hover:opacity-80 hover:underline active:opacity-40 duration-300 ease-in-out"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href={link}
-                className="hover:opacity-80 hover:underline active:opacity-40 duration-300 ease-in-out"
-              >
-                Projects
-              </a>
-            </li>
-            <li>
-              <a
-                href={link}
-                className="hover:opacity-80 hover:underline active:opacity-40 duration-300 ease-in-out"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href={link}
-                className="hover:opacity-80 hover:underline active:opacity-40 duration-300 ease-in-out"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
-          <AiOutlineSearch
-            className="sm:block hidden cursor-pointer hover:opacity-80 active:opacity-40 duration-300 ease-in-out"
-            size={25}
-          />
-        </nav>
+				<nav className="md:gap-16 md:flex hidden mt-10 md:mt-0 justify-center items-center">
+					<ul className="grid grid-cols-2 sm:grid-cols-4 md:gap-24 gap-10 p-2 text-center">
+						<li>
+							<a
+								href={link}
+								className="hover:opacity-80 hover:underline active:opacity-40 duration-300 ease-in-out"
+							>
+								Home
+							</a>
+						</li>
+						<li>
+							<a
+								href={link}
+								className="hover:opacity-80 hover:underline active:opacity-40 duration-300 ease-in-out"
+							>
+								Projects
+							</a>
+						</li>
+						<li>
+							<a
+								href={link}
+								className="hover:opacity-80 hover:underline active:opacity-40 duration-300 ease-in-out"
+							>
+								About
+							</a>
+						</li>
+						<li>
+							<a
+								href={link}
+								className="hover:opacity-80 hover:underline active:opacity-40 duration-300 ease-in-out"
+							>
+								Contact
+							</a>
+						</li>
+					</ul>
+					<AiOutlineSearch
+						className="sm:block hidden cursor-pointer hover:opacity-80 active:opacity-40 duration-300 ease-in-out"
+						size={25}
+					/>
+				</nav>
 
-        <div
-          className={
-            !nav
-              ? "fixed z-50 left-0 top-0 w-[60%] ease-in-out duration-500 h-full border-r border-r-gray-900 bg-[#2c2c2cf9]"
-              : "fixed left-[-100%] top-0 duration-500 linear h-full z-50"
-          }
-        >
-          <nav className="md:gap-16  md:hidden flex flex-col mt-10 md:mt-0 justify-center items-center">
-            <a
-              href={link}
-              className="font-[Barlow] text-[42px] font-semibold text-white mb-4"
-            >
-              ArchiWeb
-            </a>
-            <ul className="p-2 text-center">
-              <li className="mb-5 ">
-                <a
-                  href={link}
-                  className="text-white  hover:opacity-80  active:opacity-40 duration-300 ease-in-out"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="mb-5  ">
-                <a
-                  href={link}
-                  className="text-white  hover:opacity-80  active:opacity-40 duration-300 ease-in-out"
-                >
-                  Projects
-                </a>
-              </li>
-              <li className="mb-5 ">
-                <a
-                  href={link}
-                  className="text-white  hover:opacity-80  active:opacity-40 duration-300 ease-in-out"
-                >
-                  About
-                </a>
-              </li>
-              <li className="mb-5 ">
-                <a
-                  href={link}
-                  className="text-white  hover:opacity-80  active:opacity-40 duration-300 ease-in-out"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-            <AiOutlineSearch
-              className="sm:block hidden cursor-pointer hover:opacity-80 active:opacity-40 duration-300 ease-in-out"
-              size={25}
-            />
-          </nav>
-        </div>
-      </div>
-    </header>
-  );
+				<div
+					className={
+						!navIsOpen
+							? "fixed z-50 left-0 top-0 w-[60%] ease-in-out duration-500 h-full border-r border-r-gray-900 bg-[#2c2c2cf9]"
+							: "fixed left-[-100%] top-0 duration-500 linear h-full z-50"
+					}
+				>
+					<nav className="md:gap-16  md:hidden flex flex-col mt-10 md:mt-0 justify-center items-center">
+						<a
+							href={link}
+							className="font-[Barlow] text-[42px] font-semibold text-white mb-4"
+						>
+							ArchiWeb
+						</a>
+						<ul className="p-2 text-center">
+							<li className="mb-5 ">
+								<a
+									href={link}
+									className="text-white  hover:opacity-80  active:opacity-40 duration-300 ease-in-out"
+								>
+									Home
+								</a>
+							</li>
+							<li className="mb-5  ">
+								<a
+									href={link}
+									className="text-white  hover:opacity-80  active:opacity-40 duration-300 ease-in-out"
+								>
+									Projects
+								</a>
+							</li>
+							<li className="mb-5 ">
+								<a
+									href={link}
+									className="text-white  hover:opacity-80  active:opacity-40 duration-300 ease-in-out"
+								>
+									About
+								</a>
+							</li>
+							<li className="mb-5 ">
+								<a
+									href={link}
+									className="text-white  hover:opacity-80  active:opacity-40 duration-300 ease-in-out"
+								>
+									Contact
+								</a>
+							</li>
+						</ul>
+						<AiOutlineSearch
+							className="sm:block hidden cursor-pointer hover:opacity-80 active:opacity-40 duration-300 ease-in-out"
+							size={25}
+						/>
+					</nav>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,63 +9,73 @@ import projectsFive from "../img/projects/projectsFive.jpg";
 import projectsSix from "../img/projects/projectsSix.jpg";
 
 const Projects = () => {
-  const projOneTitle = [
-    "Our Services Line one Servive line two",
-    "Our Services Line one Servive line two",
-    "Our Services Line one Servive line two",
-    "Our Services Line one Servive line two",
-    "Our Services Line one Servive line two",
-    "Our Services Line one Servive line two",
-  ];
-  return (
-    <section className="projects container m-auto px-[5px] mb-[90px]">
-      <div className="flex md:flex-row flex-col md:justify-between md:mb-16">
-        <h2 className="font-['Playfair_Display'] max-md:mb-4 max-md:text-center text-5xl font-[900]">
-          Our Projects
-        </h2>
-        <ul className="flex items-center gap-6 max-md:mb-4 max-md:justify-center lg:gap-x-20 max-md:flex-wrap">
-          <li className="font-[600]">All</li>
-          <li className="">Furniture</li>
-          <li className="">Design</li>
-          <li className="">House</li>
-        </ul>
-      </div>
-      <div>
-        <ul className="grid justify-center sm:grid-cols-2 md:grid-cols-3 gap-20">
-          <ProjectsCard
-            img={projectsOne}
-            alt={projOneTitle[0]}
-            title={projOneTitle[0]}
-          />
-          <ProjectsCard
-            img={projectsTwo}
-            alt={projOneTitle[1]}
-            title={projOneTitle[1]}
-          />
-          <ProjectsCard
-            img={projectsThree}
-            alt={projOneTitle[2]}
-            title={projOneTitle[2]}
-          />
-          <ProjectsCard
-            img={projectsFour}
-            alt={projOneTitle[3]}
-            title={projOneTitle[3]}
-          />
-          <ProjectsCard
-            img={projectsFive}
-            alt={projOneTitle[4]}
-            title={projOneTitle[4]}
-          />
-          <ProjectsCard
-            img={projectsSix}
-            alt={projOneTitle[5]}
-            title={projOneTitle[5]}
-          />
-        </ul>
-      </div>
-    </section>
-  );
+	const projOneTitle = [
+		"Our Services Line one Servive line two",
+		"Our Services Line one Servive line two",
+		"Our Services Line one Servive line two",
+		"Our Services Line one Servive line two",
+		"Our Services Line one Servive line two",
+		"Our Services Line one Servive line two",
+	];
+	return (
+		<section className="projects container m-auto px-[5px] mb-[90px]">
+			<div className="flex md:flex-row flex-col md:justify-between md:mb-16">
+				<h2 className="font-['Playfair_Display'] max-md:mb-4 max-md:text-center text-5xl font-[900]">
+					Our Projects
+				</h2>
+				<ul className="flex items-center gap-6 max-md:mb-4 max-md:justify-center lg:gap-x-20 max-md:flex-wrap">
+					<li>
+						<a className="projects__active font-[600]" href="#">
+							All
+						</a>
+					</li>
+					<li>
+						<a href="#">Furniture</a>
+					</li>
+					<li>
+						<a href="#">Design</a>
+					</li>
+					<li>
+						<a href="#">House</a>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<ul className="grid justify-center sm:grid-cols-2 md:grid-cols-3 gap-20">
+					<ProjectsCard
+						img={projectsOne}
+						alt={projOneTitle[0]}
+						title={projOneTitle[0]}
+					/>
+					<ProjectsCard
+						img={projectsTwo}
+						alt={projOneTitle[1]}
+						title={projOneTitle[1]}
+					/>
+					<ProjectsCard
+						img={projectsThree}
+						alt={projOneTitle[2]}
+						title={projOneTitle[2]}
+					/>
+					<ProjectsCard
+						img={projectsFour}
+						alt={projOneTitle[3]}
+						title={projOneTitle[3]}
+					/>
+					<ProjectsCard
+						img={projectsFive}
+						alt={projOneTitle[4]}
+						title={projOneTitle[4]}
+					/>
+					<ProjectsCard
+						img={projectsSix}
+						alt={projOneTitle[5]}
+						title={projOneTitle[5]}
+					/>
+				</ul>
+			</div>
+		</section>
+	);
 };
 
 export default Projects;
